a086987b6fb1f319d4678c3a8359fda4f9bc3084
Garrett McCullough <gwmccull@gmail.com>
2016-02-01 22:34:20 -0800

    Change db variables to environment variables

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,8 @@
-import keys from '../keys/db';
-
 import mongoose from 'mongoose';
 
 export default function(callback) {
     // connect to a database if needed
-    mongoose.connect(`mongodb://${keys.username}:${keys.password}@${keys.url}`);
+    mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`);
 
     callback();
 }
